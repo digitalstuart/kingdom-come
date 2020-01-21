be87769ce00a269163b1e00ae27b2c0d95248e63
digitalstuart <stuartrichardsdigital@gmail.com>
2020-01-21 11:18:57 +0000

    Javascript and API call for basic Google Maps embed

---
+++ assets/js/map.js
@@ -0,0 +1,8 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 14,
+        center: { 
+            lat: 52.272145, lng: -9.7164645 
+        }
+        });
+}
