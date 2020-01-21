ceb8da2204816fadf32209b2eb59f7dd06849e86
digitalstuart <stuartrichardsdigital@gmail.com>
2020-01-21 14:25:29 +0000

    Tralee location added with single map marker

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,8 +1,12 @@
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 14,
-        center: { 
-            lat: 52.272145, lng: -9.7164645 
-        }
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 52.272145, lng: -9.7164645},
+          zoom: 14
         });
-}
+
+  
+  var museum = {lat: 52.2667702, lng: -9.7061565};
+  
+ 
+  var marker = new google.maps.Marker({position: museum, map: map});
+    }
