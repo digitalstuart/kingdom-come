b0486ac7037333e2edd9d30a898a974bb192b182
digitalstuart <stuartrichardsdigital@gmail.com>
2020-01-28 12:15:51 +0000

    Streamlined restaurants into single JSON file

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,39 +1,7 @@
-function loadJSON1(callback) {
+function loadJSON(callback) {
 
     let xobj = new XMLHttpRequest();
-    xobj.open('GET', 'assets/tralee_restaurants_1.json', true);
-    xobj.onreadystatechange = function() {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-
-            // .open will NOT return a value but simply returns undefined in async mode so use a callback
-            callback(xobj.responseText);
-
-        }
-    }
-    xobj.send(null);
-
-}
-
-function loadJSON2(callback) {
-
-    let xobj = new XMLHttpRequest();
-    xobj.open('GET', 'assets/tralee_restaurants_2.json', true);
-    xobj.onreadystatechange = function() {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-
-            // .open will NOT return a value but simply returns undefined in async mode so use a callback
-            callback(xobj.responseText);
-
-        }
-    }
-    xobj.send(null);
-
-}
-
-function loadJSON3(callback) {
-
-    let xobj = new XMLHttpRequest();
-    xobj.open('GET', 'assets/tralee_restaurants_3.json', true);
+    xobj.open('GET', 'assets/tralee_restaurants.json', true);
     xobj.onreadystatechange = function() {
         if (xobj.readyState == 4 && xobj.status == "200") {
 
@@ -47,35 +15,9 @@ function loadJSON3(callback) {
 }
 
 // Call to function with anonymous callback
-loadJSON1(function(response) {
-    traleeRestaurants1 = JSON.parse(response);
-    console.log(traleeRestaurants1.results[1].name);
-    
-    // Do Something with the response e.g.
-    //jsonresponse = JSON.parse(response);
-
-    // Assuming json data is wrapped in square brackets as Drew suggests
-    //console.log(jsonresponse[1].name);
-
-});
-
-// Call to function with anonymous callback
-loadJSON2(function(response) {
-    traleeRestaurants2 = JSON.parse(response);
-    console.log(traleeRestaurants2.results[1].name);
-    
-    // Do Something with the response e.g.
-    //jsonresponse = JSON.parse(response);
-
-    // Assuming json data is wrapped in square brackets as Drew suggests
-    //console.log(jsonresponse[1].name);
-
-});
-
-// Call to function with anonymous callback
-loadJSON3(function(response) {
-    traleeRestaurants3 = JSON.parse(response);
-    console.log(traleeRestaurants3.results[1].name);
+loadJSON(function(response) {
+    JSONresponse = JSON.parse(response);
+    console.log(JSONresponse.results[1].name);
     
     // Do Something with the response e.g.
     //jsonresponse = JSON.parse(response);
@@ -93,33 +35,13 @@ function initMap() {
         mapTypeControl: false
     });
        
-        for (let i = 0; i < traleeRestaurants1.results.length; i++) {
-        let lat = traleeRestaurants1.results[i].geometry.location.lat;
-        let lng = traleeRestaurants1.results[i].geometry.location.lng;
+        for (let i = 0; i < JSONresponse.results.length; i++) {
+        let lat = JSONresponse.results[i].geometry.location.lat;
+        let lng = JSONresponse.results[i].geometry.location.lng;
         let latLng = new google.maps.LatLng(lat, lng);
         let marker = new google.maps.Marker({
             position: latLng,
             map: map
     });
-    }
-
-    for (let i = 0; i < traleeRestaurants2.results.length; i++) {
-        let lat = traleeRestaurants2.results[i].geometry.location.lat;
-        let lng = traleeRestaurants2.results[i].geometry.location.lng;
-        let latLng = new google.maps.LatLng(lat, lng);
-        let marker = new google.maps.Marker({
-            position: latLng,
-            map: map
-    });
-    }
-
-    for (let i = 0; i < traleeRestaurants3.results.length; i++) {
-        let lat = traleeRestaurants3.results[i].geometry.location.lat;
-        let lng = traleeRestaurants3.results[i].geometry.location.lng;
-        let latLng = new google.maps.LatLng(lat, lng);
-        let marker = new google.maps.Marker({
-            position: latLng,
-            map: map
-    });
-    }
+    }    
 }
