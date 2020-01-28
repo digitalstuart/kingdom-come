8db72520d32dc56fdf4975d109f8f1bfb74fcceb
digitalstuart <stuartrichardsdigital@gmail.com>
2020-01-28 11:38:49 +0000

    Loaded multiple JSON files, tested console.log & added multiple map markers

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,7 +1,39 @@
-function loadJSON(callback) {
+function loadJSON1(callback) {
 
     let xobj = new XMLHttpRequest();
-    xobj.open('GET', 'assets/maps.json', true);
+    xobj.open('GET', 'assets/tralee_restaurants_1.json', true);
+    xobj.onreadystatechange = function() {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+
+            // .open will NOT return a value but simply returns undefined in async mode so use a callback
+            callback(xobj.responseText);
+
+        }
+    }
+    xobj.send(null);
+
+}
+
+function loadJSON2(callback) {
+
+    let xobj = new XMLHttpRequest();
+    xobj.open('GET', 'assets/tralee_restaurants_2.json', true);
+    xobj.onreadystatechange = function() {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+
+            // .open will NOT return a value but simply returns undefined in async mode so use a callback
+            callback(xobj.responseText);
+
+        }
+    }
+    xobj.send(null);
+
+}
+
+function loadJSON3(callback) {
+
+    let xobj = new XMLHttpRequest();
+    xobj.open('GET', 'assets/tralee_restaurants_3.json', true);
     xobj.onreadystatechange = function() {
         if (xobj.readyState == 4 && xobj.status == "200") {
 
@@ -15,9 +47,9 @@ function loadJSON(callback) {
 }
 
 // Call to function with anonymous callback
-loadJSON(function(response) {
-    jsonresponse = JSON.parse(response);
-    console.log(jsonresponse.results[1].geometry.location.lat);
+loadJSON1(function(response) {
+    traleeRestaurants1 = JSON.parse(response);
+    console.log(traleeRestaurants1.results[1].name);
     
     // Do Something with the response e.g.
     //jsonresponse = JSON.parse(response);
@@ -27,11 +59,31 @@ loadJSON(function(response) {
 
 });
 
-function mapTest () {
-    document.getElementById("test").innerHTML = jsonresponse.results[1].formatted_address;    
-}
+// Call to function with anonymous callback
+loadJSON2(function(response) {
+    traleeRestaurants2 = JSON.parse(response);
+    console.log(traleeRestaurants2.results[1].name);
+    
+    // Do Something with the response e.g.
+    //jsonresponse = JSON.parse(response);
 
-document.getElementById("button-test").onclick = function() {mapTest()};
+    // Assuming json data is wrapped in square brackets as Drew suggests
+    //console.log(jsonresponse[1].name);
+
+});
+
+// Call to function with anonymous callback
+loadJSON3(function(response) {
+    traleeRestaurants3 = JSON.parse(response);
+    console.log(traleeRestaurants3.results[1].name);
+    
+    // Do Something with the response e.g.
+    //jsonresponse = JSON.parse(response);
+
+    // Assuming json data is wrapped in square brackets as Drew suggests
+    //console.log(jsonresponse[1].name);
+
+});
 
 function initMap() {
     
@@ -41,9 +93,29 @@ function initMap() {
         mapTypeControl: false
     });
        
-        for (let i = 0; i < jsonresponse.results.length; i++) {
-        let lat = jsonresponse.results[i].geometry.location.lat;
-        let lng = jsonresponse.results[i].geometry.location.lng;
+        for (let i = 0; i < traleeRestaurants1.results.length; i++) {
+        let lat = traleeRestaurants1.results[i].geometry.location.lat;
+        let lng = traleeRestaurants1.results[i].geometry.location.lng;
+        let latLng = new google.maps.LatLng(lat, lng);
+        let marker = new google.maps.Marker({
+            position: latLng,
+            map: map
+    });
+    }
+
+    for (let i = 0; i < traleeRestaurants2.results.length; i++) {
+        let lat = traleeRestaurants2.results[i].geometry.location.lat;
+        let lng = traleeRestaurants2.results[i].geometry.location.lng;
+        let latLng = new google.maps.LatLng(lat, lng);
+        let marker = new google.maps.Marker({
+            position: latLng,
+            map: map
+    });
+    }
+
+    for (let i = 0; i < traleeRestaurants3.results.length; i++) {
+        let lat = traleeRestaurants3.results[i].geometry.location.lat;
+        let lng = traleeRestaurants3.results[i].geometry.location.lng;
         let latLng = new google.maps.LatLng(lat, lng);
         let marker = new google.maps.Marker({
             position: latLng,
