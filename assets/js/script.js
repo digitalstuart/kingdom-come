e24cb86ae6923acdf0e8c26fde24a85f80c5d495
digitalstuart <stuartrichardsdigital@gmail.com>
2020-02-06 14:05:34 +0000

    Changed alt tags for various 'three things' images

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,9 +22,12 @@ document.getElementById("kenmare-link").addEventListener("click", function () { 
 document.getElementById("kenmare-link").addEventListener("click", function () { document.getElementById("quote-two").innerHTML = quotesObject.quotesString[4].quote });
 document.getElementById("kenmare-link").addEventListener("click", function () { document.getElementById("who-two").innerHTML = quotesObject.quotesString[4].who });
 document.getElementById("kenmare-link").addEventListener("click", function () { document.getElementById("quote-three").innerHTML = quotesObject.quotesString[5].quote + " " + quotesObject.quotesString[5].who });
-document.getElementById("kenmare-link").addEventListener("click", function () { document.getElementById("three-things-one").src = 'assets/images/kenmare-reenagross.jpg' });
+document.getElementById("kenmare-link").addEventListener("click", function () { document.getElementById("three-things-one").src = 'assets/images/kenmare-reenagross.jpg'});
 document.getElementById("kenmare-link").addEventListener("click", function () { document.getElementById("three-things-two").src = 'assets/images/kenmare-square.jpg' });
 document.getElementById("kenmare-link").addEventListener("click", function () { document.getElementById("three-things-three").src = 'assets/images/kenmare-main.jpg' });
+document.getElementById("kenmare-link").addEventListener("click", function () { document.getElementById("three-things-one").alt = 'Reenagross Park in Kenmare'});
+document.getElementById("kenmare-link").addEventListener("click", function () { document.getElementById("three-things-two").alt = 'The Square in Kenmare' });
+document.getElementById("kenmare-link").addEventListener("click", function () { document.getElementById("three-things-three").alt = 'Main Street in Kenmare' });
 document.getElementById("kenmare-link").addEventListener("click", function () { document.getElementById("text-one").innerHTML = 'Reenagross' });
 document.getElementById("kenmare-link").addEventListener("click", function () { document.getElementById("text-two").innerHTML = 'Square' });
 document.getElementById("kenmare-link").addEventListener("click", function () { document.getElementById("text-three").innerHTML = 'Main St' });
@@ -37,6 +40,9 @@ document.getElementById("killarney-link").addEventListener("click", function () 
 document.getElementById("killarney-link").addEventListener("click", function () { document.getElementById("three-things-one").src = 'assets/images/killarney-muckross.jpg' });
 document.getElementById("killarney-link").addEventListener("click", function () { document.getElementById("three-things-two").src = 'assets/images/killarney-horses.jpg' });
 document.getElementById("killarney-link").addEventListener("click", function () { document.getElementById("three-things-three").src = 'assets/images/killarney-town.jpg' });
+document.getElementById("killarney-link").addEventListener("click", function () { document.getElementById("three-things-one").alt = 'Muckross House in Killarney' });
+document.getElementById("killarney-link").addEventListener("click", function () { document.getElementById("three-things-two").alt = 'Jaunting cars in Killarney' });
+document.getElementById("killarney-link").addEventListener("click", function () { document.getElementById("three-things-three").alt = 'Killarney town centre' });
 document.getElementById("killarney-link").addEventListener("click", function () { document.getElementById("text-one").innerHTML = 'Muckross' });
 document.getElementById("killarney-link").addEventListener("click", function () { document.getElementById("text-two").innerHTML = 'Jaunting' });
 document.getElementById("killarney-link").addEventListener("click", function () { document.getElementById("text-three").innerHTML = 'Town' });
@@ -48,6 +54,9 @@ document.getElementById("listowel-link").addEventListener("click", function () {
 document.getElementById("listowel-link").addEventListener("click", function () { document.getElementById("three-things-one").src = 'assets/images/listowel-monorail.jpg' });
 document.getElementById("listowel-link").addEventListener("click", function () { document.getElementById("three-things-two").src = 'assets/images/listowel-castle.jpg' });
 document.getElementById("listowel-link").addEventListener("click", function () { document.getElementById("three-things-three").src = 'assets/images/listowel-garden.jpg' });
+document.getElementById("listowel-link").addEventListener("click", function () { document.getElementById("three-things-one").alt = 'Lartigue Monorail in Listowel' });
+document.getElementById("listowel-link").addEventListener("click", function () { document.getElementById("three-things-two").alt = 'Listowel Castle' });
+document.getElementById("listowel-link").addEventListener("click", function () { document.getElementById("three-things-three").alt = 'Gardens of Europe in Listowel' });
 document.getElementById("listowel-link").addEventListener("click", function () { document.getElementById("text-one").innerHTML = 'Monorail' });
 document.getElementById("listowel-link").addEventListener("click", function () { document.getElementById("text-two").innerHTML = 'Castle' });
 document.getElementById("listowel-link").addEventListener("click", function () { document.getElementById("text-three").innerHTML = 'Gardens' });
@@ -59,6 +68,9 @@ document.getElementById("tralee-link").addEventListener("click", function () { d
 document.getElementById("tralee-link").addEventListener("click", function () { document.getElementById("three-things-one").src = 'assets/images/tralee-blennerville.jpg' });
 document.getElementById("tralee-link").addEventListener("click", function () { document.getElementById("three-things-two").src = 'assets/images/tralee-park.jpg' });
 document.getElementById("tralee-link").addEventListener("click", function () { document.getElementById("three-things-three").src = 'assets/images/tralee-aquadome.jpg' });
+document.getElementById("tralee-link").addEventListener("click", function () { document.getElementById("three-things-one").alt = 'Blennerville Windmill in Tralee' });
+document.getElementById("tralee-link").addEventListener("click", function () { document.getElementById("three-things-two").alt = 'Tralee town park' });
+document.getElementById("tralee-link").addEventListener("click", function () { document.getElementById("three-things-three").alt = 'Aquadome swimming pool in Tralee' });
 document.getElementById("tralee-link").addEventListener("click", function () { document.getElementById("text-one").innerHTML = 'Blennerville' });
 document.getElementById("tralee-link").addEventListener("click", function () { document.getElementById("text-two").innerHTML = 'Park' });
 document.getElementById("tralee-link").addEventListener("click", function () { document.getElementById("text-three").innerHTML = 'Aquadome' });
@@ -70,6 +82,9 @@ document.getElementById("dingle-link").addEventListener("click", function () { d
 document.getElementById("dingle-link").addEventListener("click", function () { document.getElementById("three-things-one").src = 'assets/images/dingle-fungie.jpg' });
 document.getElementById("dingle-link").addEventListener("click", function () { document.getElementById("three-things-two").src = 'assets/images/dingle-inch.jpg' });
 document.getElementById("dingle-link").addEventListener("click", function () { document.getElementById("three-things-three").src = 'assets/images/dingle-oceanworld.jpg' });
+document.getElementById("dingle-link").addEventListener("click", function () { document.getElementById("three-things-one").alt = 'Statue of Fungie the dolphin in Dingle' });
+document.getElementById("dingle-link").addEventListener("click", function () { document.getElementById("three-things-two").alt = 'Inch Beach on the Dingle Peninsula' });
+document.getElementById("dingle-link").addEventListener("click", function () { document.getElementById("three-things-three").alt = 'Tunnel aquarium at Dingle Oceanworld' });
 document.getElementById("dingle-link").addEventListener("click", function () { document.getElementById("text-one").innerHTML = 'Fungie' });
 document.getElementById("dingle-link").addEventListener("click", function () { document.getElementById("text-two").innerHTML = 'Inch' });
 document.getElementById("dingle-link").addEventListener("click", function () { document.getElementById("text-three").innerHTML = 'Oceanworld' });
