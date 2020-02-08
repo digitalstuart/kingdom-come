f7470d834e0be00a37b0f05a07b4ebf2b5db292c
digitalstuart <stuartrichardsdigital@gmail.com>
2020-02-08 11:38:20 +0000

    Kenmare map markers completed

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,11 +1,3 @@
-/*
-
-let restaurantMarkers = [];
-let hotelMarkers = [];
-let pubMarkers = [];
-
-*/
-
 let eating = '{ "eatingString" : [' +
     '{ "formatted_address": "Green Street, Dingle, County Kerry, Ireland" , "lat": 52.1415429 , "lng": -10.2704251 , "name": "The Fish Box/Flannery\'s Seafood Bar" , "rating": 5.0 , "user_ratings_total": 500},' +
     '{ "formatted_address": "Main Street, Grove, Dingle, County Kerry, Ireland" , "lat" : 52.1415871 , "lng" : -10.2686796, "name" : "Land To Sea", "rating" : 5.0, "user_ratings_total" : 89},' +
@@ -27,33 +19,26 @@ let staying = '{ "stayingString" : [' +
     '{ "formatted_address": "Gortonora, Dingle, County Kerry, Ireland" , "lat" : 52.1381733 , "lng" : -10.262914 , "name" : "Greenmount House", "rating" : 5.0, "user_ratings_total" : 1080},' +
     '{ "formatted_address": "The Wood, Dingle, County Kerry, Ireland" , "lat" : 52.14006089999999 , "lng" : -10.2889296, "name" : "Milltown House", "rating" : 5.0, "user_ratings_total" : 631} ]}';
 
-/*
+let kenmareEating = '{ "kenmareEatingString" : [' +
+    '{ "formatted_address": "35 Main Street, Kenmare, County Kerry, Ireland" , "lat": 51.87956399999999 , "lng": -9.582127 , "name": "No. 35" , "rating": 4.5 , "user_ratings_total": 931},' +
+    '{ "formatted_address": "Main Street, Kenmare, County Kerry, Ireland" , "lat" : 51.8796869 , "lng" : -9.582380499999999, "name" : "Mulcahy\'s", "rating" : 4.5, "user_ratings_total" : 670},' +
+    '{ "formatted_address": "36 Henry Street, Kenmare, County Kerry, Ireland" , "lat" : 51.8783192 , "lng" : -9.5834943, "name" : "Mick & Jimmy\'s", "rating" : 4.5, "user_ratings_total" : 716},' +
+    '{ "formatted_address": "26 Main Street, Kenmare, County Kerry, Ireland" , "lat" : 51.879229 , "lng" : -9.581258099999999, "name" : "Lily House Thai & Chinese Restaurant", "rating" : 4.5, "user_ratings_total" : 212},' +
+    '{ "formatted_address": "Main Street, Kenmare, County Kerry, Ireland" , "lat" : 51.8791641, "lng" : -9.5811589, "name" : "Tom Crean Fish & Wine", "rating" : 4.5, "user_ratings_total" : 709} ]}';
 
-let hotels = '{ "hotelsString" : [' +
-    '{ "formatted_address": "V92 W56F, Maine St, Tralee, Co. Kerry, Ireland", "lat" : 52.2707571, "lng" : -9.7046674, "name": "The Ashe Hotel", "rating": 4.5, "user_ratings_total": 782 , "category": "hotels"},' +
-    '{ "formatted_address": "Dan Spring Rd, Cloon Beg, Tralee, Co. Kerry, V92 HKA4, Ireland", "lat" : 52.2635733, "lng" : -9.700669299999999, "name" : "The Rose Hotel", "rating" : 4.5, "user_ratings_total" : 1353 , "category": "hotels"},' +
-    '{ "formatted_address": "Denny St, Tralee, Co. Kerry, Ireland", "lat" : 52.2689479, "lng" : -9.705022699999999, "name" : "The Grand Hotel", "rating" : 4.4, "user_ratings_total" : 855 , "category": "hotels"},' +
-    '{ "formatted_address": "Prince\'s St, Tralee, Co. Kerry, Ireland", "lat" : 52.2671005, "lng" : -9.710116099999999, "name" : "Brandon Hotel & Spa", "rating" : 3.9, "user_ratings_total" : 662 , "category": "hotels"},' +
-    '{ "formatted_address": "Oakpark, Tralee, Co. Kerry, Ireland", "lat" : 52.276176, "lng" : -9.691155, "name" : "Meadowlands Hotel", "rating" : 4.4, "user_ratings_total" : 619 , "category": "hotels"},' +
-    '{ "formatted_address": "27 Denny St, Tralee, Co. Kerry, Ireland", "lat" : 52.268481, "lng" : -9.705209, "name" : "Imperial Hotel Tralee", "rating" : 4.2, "user_ratings_total" : 325 , "category": "hotels"},' +
-    '{ "formatted_address": "Ratass, Tralee, Co. Kerry, Ireland", "lat" : 52.2640488, "lng" : -9.6762406, "name" : "Manor West Hotel & Leisure Club", "rating" : 4.4, "user_ratings_total" : 710 , "category": "hotels"},' +
-    '{ "formatted_address": "Upper Castle Street, Cloonalour, Tralee, Co. Kerry, Ireland", "lat" : 52.2689218, "lng" : -9.702276099999999, "name" : "Benners Hotel Tralee", "rating" : 4.3, "user_ratings_total" : 412 , "category": "hotels"},' +
-    '{ "formatted_address": "Leebrook, Tralee, Co. Kerry, V92 W279, Ireland", "lat" : 52.26106499999999, "lng" : -9.660157999999999, "name" : "Ballygarry House Hotel and Spa", "rating" : 4.7, "user_ratings_total" : 1111 , "category": "hotels"},' +
-    '{ "formatted_address": "Mary St, Tralee, Co. Kerry, Ireland", "lat" : 52.26782499999999, "lng" : -9.7080758, "name" : "Tralee Holiday Lodge Guest Accommodation", "rating" : 3.8, "user_ratings_total" : 204 , "category": "hotels"} ]}';
+let kenmareAttractions = '{ "kenmareAttractionsString" : [' +
+    '{ "formatted_address": "Raleigh Bonane, Kenmare, County Kerry, Ireland" , "lat" : 51.8012086, "lng" : -9.562438799999999 , "name": "Molly Gallivan\'s Cottage & Traditional Farm" , "rating": 4.5 , "user_ratings_total": 983},' +
+    '{ "formatted_address": "Gleninchaquin, Kenmare, County Kerry, Ireland" , "lat" : 51.80198499999999, "lng" : -9.660639, "name" : "Gleninchaquin Park", "rating" : 4.5, "user_ratings_total" : 670},' +
+    '{ "formatted_address": "Killowen, Kenmare, County Kerry, Ireland" , "lat" : 51.8765274, "lng" : -9.579175599999999, "name" : "Reenagross Park", "rating" : 4.5, "user_ratings_total" : 73},' +
+    '{ "formatted_address": "52 Market Street, Kenmare, County Kerry, Ireland" , "lat" : 51.8784747, "lng" : -9.5881297 , "name" : "Kenmare Stone Circle", "rating" : 4.0, "user_ratings_total" : 614},' +
+    '{ "formatted_address": "5 Bridge Street, Kenmare, County Kerry, Ireland" , "lat" : 51.8803966, "lng" : -9.586065999999999, "name" : "Cromwell\'s Bridge", "rating" : 4.0, "user_ratings_total" : 144} ]}';
 
-let pubs = '{ "pubsString" : [' +
-    '{ "formatted_address": "30 Lower Castle St, Cloonalour, Tralee, Co. Kerry, V92 N9FT, Ireland", "lat" : 52.26921300000001, "lng" : -9.703892999999999, "name": "Baily\'s Corner", "rating": 4.6, "user_ratings_total": 348 , "category": "pubs"},' +
-    '{ "formatted_address": "18 The Mall, Gallowsfields, Tralee, Co. Kerry, V92 DD5X, Ireland", "lat" : 52.2694564, "lng" : -9.704781100000002, "name" : "Paddy Mac\'s Pub", "rating" : 4.5, "user_ratings_total" : 205 , "category": "pubs"},' +
-    '{ "formatted_address": "27 The Mall, Balloonagh, Tralee, Co. Kerry, V92 HN40, Ireland", "lat" : 52.2696373, "lng" : -9.705735299999999, "name" : "An Teach Beag", "rating" : 4.3, "user_ratings_total" : 133 , "category": "pubs"},' +
-    '{ "formatted_address": "Rock St, Balloonagh, Tralee, Co. Kerry, Ireland", "lat" : 52.2701812, "lng" : -9.708096899999999, "name" : "Kirbys Brogue Inn", "rating" : 4.6, "user_ratings_total" : 1158 , "category": "pubs"},' +
-    '{ "formatted_address": "41 Bridge St, Tralee, Co. Kerry, Ireland", "lat" : 52.26920399999999, "lng" : -9.708067999999999, "name" : "Sean Ogs Bar & B&B", "rating" : 4.6, "user_ratings_total" : 432 , "category": "pubs"},' +
-    '{ "formatted_address": "77 Rock St, Gallowsfields, Tralee, Co. Kerry, Ireland", "lat" : 52.2721216, "lng" : -9.7073798, "name" : "The Castle Bar", "rating" : 4.6, "user_ratings_total" : 120 , "category": "pubs"},' +
-    '{ "formatted_address": "22 Castle St, Tralee, Co. Kerry, Ireland", "lat" : 52.2691403, "lng" : -9.702966999999999, "name" : "Turners Bar", "rating" : 4.4, "user_ratings_total" : 160 , "category": "pubs"},' +
-    '{ "formatted_address": "28 Lower Castle St, Cloonalour, Tralee, Co. Kerry, V92 AD60, Ireland", "lat" : 52.2692419, "lng" : -9.703685199999999, "name" : "The Blasket Bar", "rating" : 4.4, "user_ratings_total" : 292 , "category": "pubs"},' +
-    '{ "formatted_address": "5 Brogue maker\'s Ln, Gallowsfields, Tralee, Co. Kerry, V92 NA46, Ireland", "lat" : 52.2706652, "lng" : -9.7054466, "name" : "Roundy\'s Bar", "rating" : 4.4, "user_ratings_total" : 87 , "category": "pubs"},' +
-    '{ "formatted_address": "Pembroke St, Balloonagh, Tralee, Co. Kerry, Ireland", "lat" : 52.2715185, "lng" : -9.708596499999999, "name" : "The Greyhound Bar", "rating" : 4.6, "user_ratings_total" : 198 , "category": "pubs"} ]}';
-
-*/
+let kenmareStaying = '{ "kenmareStayingString" : [' +
+    '{ "formatted_address": "Casey\'s, Killarney Road, Kenmare, County Kerry, Ireland" , "lat" : 51.8835 , "lng" : -9.5915149 , "name": "Brook Lane Hotel" , "rating": 4.5 , "user_ratings_total": 1433},' +
+    '{ "formatted_address": "Sheen Falls Lodge, Kenmare, County Kerry, Ireland" , "lat" : 51.8734343 , "lng" : -9.5639118 , "name" : "Sheen Falls Lodge" , "rating" : 4.5 , "user_ratings_total" : 1265},' +
+    '{ "formatted_address": "Shelbourne Street, Kenmare, County Kerry, Ireland" , "lat" : 51.87827 , "lng" : -9.581225 , "name" : "Park Hotel Kenmare" , "rating" : 4.5 , "user_ratings_total" : 1050},' +
+    '{ "formatted_address": "Henry Street, Kenmare, County Kerry, Ireland" , "lat" : 51.8795254 , "lng" : -9.583607599999999 , "name" : "Coachmans Townhouse Hotel" , "rating" : 4.5 , "user_ratings_total" : 314},' +
+    '{ "formatted_address": "Sneem Road, Kenmare, County Kerry, Ireland" , "lat" : 51.883027 , "lng" : -9.591343 , "name" : "Kenmare Bay Hotel & Resort" , "rating" : 4.0 , "user_ratings_total" : 1523} ]}';
 
 function initMap() {
 
@@ -177,16 +162,16 @@ function initMap() {
             styles: myStyles
         });
 
-        let eatingObject = JSON.parse(eating);
-        let attractionsObject = JSON.parse(attractions);
-        let stayingObject = JSON.parse(staying);
-        let eatingImage = 'http://www.myiconfinder.com/uploads/iconsets/256-256-f900504cdc9f243b1c6852985c35a7f7.png';
+        let kenmareEatingObject = JSON.parse(kenmareEating);
+        let kenmareAttractionsObject = JSON.parse(kenmareAttractions);
+        let kenmareStayingObject = JSON.parse(kenmareStaying);
+        let eatingImage = 'https://i.imgur.com/M2nsXS1.png?1';
         let attractionsImage = 'https://i.imgur.com/QlNM9nx.png?1';
         let stayingImage = 'https://i.imgur.com/z0wt3rY.png?1';
 
-        for (let i = 0; i < eatingObject.eatingString.length; i++) {
-            let lat = eatingObject.eatingString[i].lat;
-            let lng = eatingObject.eatingString[i].lng;
+        for (let i = 0; i < kenmareEatingObject.kenmareEatingString.length; i++) {
+            let lat = kenmareEatingObject.kenmareEatingString[i].lat;
+            let lng = kenmareEatingObject.kenmareEatingString[i].lng;
             let latLng = new google.maps.LatLng(lat, lng);
             let eatingMarker = new google.maps.Marker({
                 position: latLng,
@@ -195,7 +180,7 @@ function initMap() {
                 icon: eatingImage
             });
 
-            let mapContent = eatingObject.eatingString[i].formatted_address + " " + eatingObject.eatingString[i].name;
+            let mapContent = kenmareEatingObject.kenmareEatingString[i].formatted_address + " " + kenmareEatingObject.kenmareEatingString[i].name;
             let infowindow = new google.maps.InfoWindow({
                 content: mapContent
             });
@@ -210,9 +195,9 @@ function initMap() {
 
         };
 
-        for (let i = 0; i < attractionsObject.attractionsString.length; i++) {
-            let lat = attractionsObject.attractionsString[i].lat;
-            let lng = attractionsObject.attractionsString[i].lng;
+        for (let i = 0; i < kenmareAttractionsObject.kenmareAttractionsString.length; i++) {
+            let lat = kenmareAttractionsObject.kenmareAttractionsString[i].lat;
+            let lng = kenmareAttractionsObject.kenmareAttractionsString[i].lng;
             let latLng = new google.maps.LatLng(lat, lng);
             let attractionsMarker = new google.maps.Marker({
                 position: latLng,
@@ -221,7 +206,7 @@ function initMap() {
                 icon: attractionsImage
             });
 
-            let mapContent = attractionsObject.attractionsString[i].formatted_address + " " + attractionsObject.attractionsString[i].name;
+            let mapContent = kenmareAttractionsObject.kenmareAttractionsString[i].formatted_address + " " + kenmareAttractionsObject.kenmareAttractionsString[i].name;
             let infowindow = new google.maps.InfoWindow({
                 content: mapContent
             });
@@ -235,9 +220,9 @@ function initMap() {
             });
         };
 
-        for (let i = 0; i < stayingObject.stayingString.length; i++) {
-            let lat = stayingObject.stayingString[i].lat;
-            let lng = stayingObject.stayingString[i].lng;
+        for (let i = 0; i < kenmareStayingObject.kenmareStayingString.length; i++) {
+            let lat = kenmareStayingObject.kenmareStayingString[i].lat;
+            let lng = kenmareStayingObject.kenmareStayingString[i].lng;
             let latLng = new google.maps.LatLng(lat, lng);
             let stayingMarker = new google.maps.Marker({
                 position: latLng,
@@ -246,7 +231,7 @@ function initMap() {
                 icon: stayingImage
             });
 
-            let mapContent = stayingObject.stayingString[i].formatted_address + " " + stayingObject.stayingString[i].name;
+            let mapContent = kenmareStayingObject.kenmareStayingString[i].formatted_address + " " + kenmareStayingObject.kenmareStayingString[i].name;
             let infowindow = new google.maps.InfoWindow({
                 content: mapContent
             });
@@ -286,7 +271,7 @@ function initMap() {
         let eatingObject = JSON.parse(eating);
         let attractionsObject = JSON.parse(attractions);
         let stayingObject = JSON.parse(staying);
-        let eatingImage = 'http://www.myiconfinder.com/uploads/iconsets/256-256-f900504cdc9f243b1c6852985c35a7f7.png';
+        let eatingImage = 'https://i.imgur.com/M2nsXS1.png?1';
         let attractionsImage = 'https://i.imgur.com/QlNM9nx.png?1';
         let stayingImage = 'https://i.imgur.com/z0wt3rY.png?1';
 
@@ -392,7 +377,7 @@ function initMap() {
         let eatingObject = JSON.parse(eating);
         let attractionsObject = JSON.parse(attractions);
         let stayingObject = JSON.parse(staying);
-        let eatingImage = 'http://www.myiconfinder.com/uploads/iconsets/256-256-f900504cdc9f243b1c6852985c35a7f7.png';
+        let eatingImage = 'https://i.imgur.com/M2nsXS1.png?1';
         let attractionsImage = 'https://i.imgur.com/QlNM9nx.png?1';
         let stayingImage = 'https://i.imgur.com/z0wt3rY.png?1';
 
@@ -498,7 +483,7 @@ function initMap() {
         let eatingObject = JSON.parse(eating);
         let attractionsObject = JSON.parse(attractions);
         let stayingObject = JSON.parse(staying);
-        let eatingImage = 'http://www.myiconfinder.com/uploads/iconsets/256-256-f900504cdc9f243b1c6852985c35a7f7.png';
+        let eatingImage = 'https://i.imgur.com/M2nsXS1.png?1';
         let attractionsImage = 'https://i.imgur.com/QlNM9nx.png?1';
         let stayingImage = 'https://i.imgur.com/z0wt3rY.png?1';
 
